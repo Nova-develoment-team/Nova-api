e80f562af138dd7f0a3dbb483d0edd7e07b16e32
discord tutorial <92290826+duckytutorials@users.noreply.github.com>
2022-03-30 10:52:14 +0630

    update

--- assets/keys.js
+++ assets/keys.js
@@ -2,14 +2,9 @@
 // const db = new Database();
 module.exports = {
   keys: [
-<<<<<<< HEAD
     "key here", // Owner key aka master key
     "key here" // Tester and developer key
              // Testing key here
-=======
-    "key1",
-    "ke2"
   ]
->>>>>>> 7f5e7a877c07f9e46fc33e95a674dd498a06bba8
 }
 // yall have leaked my API keys, this means war.
--- index.js
+++ index.js
@@ -6,21 +6,27 @@ const joke = require("./assets/dadjokes.json")
 const num = require("./assets/num.json")
 const db = require('quick.db')
 const keysList = require('./assets/keys.js').keys
+const { Canvas } = require('canvas-constructor/cairo')
+const canvas = require('canvas')
 
 console.log('Home page loaded')
 console.log('8ball api loaded')
 console.log('Number gen api loaded')
 console.log('gay api loaded')
 console.log('joke api loaded')
+console.log('twiter api loaded')
 
 const max = 100
 
 const app = express();
+
+require("./routes")(app)
+
 const PORT = 5377
 
 app.use(bodyParser.json());
 
-app.get('/8ball', (req, res) => {      
+app.get('/fun/8ball', (req, res) => {      
 console.log('Someone put an invalid key in the 8ball api')
 const {key} = req.query;
 if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
@@ -29,7 +35,7 @@ if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
     res.json({ response: rand_deeznuts })
 });
 
-app.get('/gay', (req, res) => {   
+app.get('/fun/gay', (req, res) => {   
 console.log('Someone put an invalid key in the gay api')    
     
 const {key} = req.query;
@@ -39,7 +45,7 @@ if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
     res.json({ response: rand_gay })
 });
 
-app.get('/joke', (req, res) => {       
+app.get('/fun/joke', (req, res) => {       
 console.log('Someone put an invalid key in the joke api')
     
 const {key} = req.query;
@@ -50,8 +56,8 @@ if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
 });
 
 
-app.get('/num', (req, res) => { 
-console.log('Someone put an invalid key in the 8ball api')
+app.get('/misc/num', (req, res) => { 
+console.log('Someone put an invalid key in the num api')
     
 const {key} = req.query;
 if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
@@ -60,14 +66,42 @@ if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
     res.json({ response: rand_num })
 });
 
+app.get('/fun/twiter/:feed', async (req, res) => {
+
+    console.log('Someone put an invalid key in the twiter api')
+    
+const {key} = req.query;
+if (!key || !keysList.includes(key)) return res.json({ error: "invalid key" })
+    
+    const img = await canvas.loadImage('https://teckspace.files.wordpress.com/2011/08/twitter1.jpg')
+
+    let image = new Canvas(550, 267)
+    .printImage(img, 0, 0, 550, 267)
+    .setTextFont('28px Impact')
+    .printText(req.params.feed, 40, 145)
+    .toBuffer();
+
+    res.set({'Content-Type': 'image/png'})//setting content type as png image!
+    res.send(image)//sending the image!
+
+});
+
 app.get('/', (req, res) => {
 	res.sendFile('index.html', {root: __dirname })
 });
-
-require("./routes")(app)
 
 app.get('/*',(req,res)=>{
 res.send({error:"Hmmm invald endpoint"})
 })
 
-app.listen(PORT, () => console.log("Nova Api is on and linked and running on https://api.nova-bot.tk")) 
+app.listen(PORT, () => {
+  console.log("--------------- [ Staticstics ] ---------------")
+  console.log("[ NodeJS: ${nodejsv}                          ]")
+  console.log("[ ------------------------------------------- ]")
+  console.log("[ Successfully loaded all elements and codes  ]")
+  console.log("[ ------------------------------------------- ]")
+  console.log("[ Nova api is up! ✅                           ]")
+  console.log("[ ------------------------------------------- ]");
+  console.log("[ Connected to - https://api.nova-bot.tk ✅    ]");
+  console.log("-----------------------------------------------");
+})
